249d470849faab9e518d2575165b34a662cd8c41
Manuel Calzado Vidal <mcalzado@gmail.com>
2024-10-23 14:47:12 +0200

    Define placehandler properties

--- handler.py
+++ handler.py
@@ -1,3 +1,17 @@
 class PlaceHandler():
-  def __init__(self, kwars:Any):
+  size:int
+  seed:int
+  citadel:bool
+  urban_castle:bool
+  plaza:bool
+  temple:bool
+  walls:bool
+  shantytown:bool
+  coast:bool
+  river:bool
+  greens:bool
+  gates:bool
+  hub:bool
+  
+  def __init__(self, url):
     pass
