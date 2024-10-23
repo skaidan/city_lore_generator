dde687c7e231e3aaaea2be8c136d2fd4f5579b63
Manuel Calzado Vidal <mcalzado@gmail.com>
2024-10-23 16:01:23 +0200

    Load PlaceHandler from url

--- handler.py
+++ handler.py
@@ -1,3 +1,5 @@
+URL_BASE = "https://watabou.github.io/city-generator/?"
+
 class PlaceHandler():
   size:int
   seed:int
@@ -13,5 +15,22 @@ class PlaceHandler():
   gates:bool
   hub:bool
   
-  def __init__(self, url):
-    pass
+  def __init__(self, url:str):
+    errors = []
+    parameters = {"size": self.size, "seed": self.seed}
+
+    url_without_base = url.removeprefix(URL_BASE)
+    if url == url_without_base:
+      errors.append(f"La url no contiene la base {URL_BASE}")
+
+    for parameter in parameters.keys:
+      if parameter in url_without_base:
+        parameter_url = (url_without_base+'.')[1]
+        value = parameter_url.split(parameters[parameter])[1].split('&')[0]
+        if value is '1':
+          parameters[parameter] = True
+        else:
+          parameters[parameter] = False
+  
+  
+    
