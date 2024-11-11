53d9bbddabc1618a06ba31660818a13607e45389
Manuel Calzado Vidal <mcalzado@gmail.com>
2024-11-11 22:38:23 +0100

    first view

--- citylore/urls.py
+++ citylore/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from views import AboutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lore', AboutView.as_view())
 ]
