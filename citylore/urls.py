f1080973e63b605c91f819ea11a186b7a8a3b334
Manuel Calzado Vidal <mcalzado@gmail.com>
2024-11-12 09:05:17 +0100

    async to hello view

--- citylore/urls.py
+++ citylore/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from citylore.views import AsyncView
+from citylore.views import HelloView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lore', AsyncView.as_view())
+    path('lore', HelloView.as_view())
 ]
--- citylore/views.py
+++ citylore/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.views import View
 
 
-class AsyncView(View):
+class HelloView(View):
     def get(self, request, *args, **kwargs):
-        # Perform io-blocking view logic using await, sleep for example.
-        return HttpResponse("Hello async world!")
+        return HttpResponse("Hello world!")
