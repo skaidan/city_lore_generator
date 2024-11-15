6da1604e27bb1cdd56d7d8c2523e5e65eac71308
Manuel Calzado Vidal <mcalzado@gmail.com>
2024-11-15 13:08:42 +0100

    path

--- citylore/views.py
+++ citylore/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 class AboutView(TemplateView):
     template_name = "about.html"
 
