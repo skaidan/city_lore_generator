f090ad75ff1823c331bfec9ca55391c14787f996
Manuel Calzado Vidal <mcalzado@gmail.com>
2024-11-15 19:29:41 +0100

    citylore

--- citylore/urls.py
+++ citylore/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from citylore.views import HelloView, AboutView
+from citylore.views import HelloView, AboutView, CityURL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
